f7a1ceabdf4c4018157354a05028bb3302efac32
tokasson <42979720+tokasson@users.noreply.github.com>
2020-06-30 13:37:50 +0200

    update code
    working main menu creator

---
+++ ConsoleMenuBuilder.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleMenuBuilder
+{
+    class ConsoleMenuBuilder
+    {
+        private int _menuEntryCount;
+
+        // HEADER DEFINITIONS
+        private char _headerDividerSymbol = '#';
+        public char HeaderDividerSymbol {
+            get => _headerDividerSymbol;
+            set => _headerDividerSymbol = value;
+        }
+
+        private int _headerCharacterWidth = 81;
+        public int HeaderCharacterWidth {
+            get => _headerCharacterWidth;
+            set => _headerCharacterWidth = value;
+        }
+
+        private string _headerDivider = "";
+        public string HeaderDivider {
+            get {
+                _headerDivider = _headerDivider == "" ? new string(_headerDividerSymbol, _headerCharacterWidth) : _headerDivider;
+                return _headerDivider;
+            }
+        }
+
+        private string _headerDividerSpace = "";
+        public string HeaderDividerSpace {
+            get {
+                _headerDividerSpace = _headerDividerSpace == "" ? _headerDividerSymbol + new string(' ', _headerCharacterWidth - 2) + _headerDividerSymbol : _headerDividerSpace;
+                return _headerDividerSpace;
+            }
+        }
+
+        // MENU DICTIONARY
+        private Dictionary<string, Menu> _menuCollection = new Dictionary<string, Menu>();
+        public bool AddMenu(string menuName, string headerText = "") {
+            if (!_menuCollection.ContainsKey(menuName)) {
+                headerText = headerText == "" ? menuName.ToUpper() : headerText.ToUpper();
+                _menuCollection.Add(menuName, new Menu(headerText));
+                Console.WriteLine(menuName + " added: " + _menuCollection["main"].HeaderText);
+            } else {
+                return false;
+            }
+            return true;
+        }
+        public string GetMenu(string menuName) {
+            string headerTitleStart = HeaderDividerSymbol + " " + _menuCollection[menuName].HeaderText;
+            string headerTitleEnd = new string(' ', _headerCharacterWidth - headerTitleStart.Length - 1) + _headerDividerSymbol;
+            string menuFullText = HeaderDivider + "\n"
+                + HeaderDividerSpace + "\n"
+                + headerTitleStart + headerTitleEnd + "\n"
+                + HeaderDividerSpace + "\n"
+                + HeaderDivider;
+            return menuFullText;
+        }
+
+        // constructors
+        public ConsoleMenuBuilder() {
+            _menuEntryCount = 0;
+        }
+
+        // methods
+        public bool AddMenuItem() {
+            return true;
+        }
+        public int Count() {
+            return _menuEntryCount;
+        }
+    }
+
+    class Menu
+    {
+        private string _headerText = "NO TEXT SET YET";
+        public string HeaderText { 
+            get => _headerText; 
+            set => _headerText = value; 
+        }       
+        public Menu(string headerText) {
+            _headerText = headerText;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleMenuBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleMenuBuilder cmb = new ConsoleMenuBuilder();
+            if (cmb.AddMenu("main", "my own header for main")) Console.WriteLine("Successfully added main!");
+
+            Console.WriteLine(cmb.HeaderDivider);
+            Console.WriteLine(cmb.GetMenu("main"));
+        }
+    }
+}
